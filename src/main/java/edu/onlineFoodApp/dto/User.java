1ed64c8963ec3e6252d275f5c2fa5533a5ef6b13
godwin845 <godwinraj845@gmail.com>
2024-12-26 16:04:42 +0530

    Updated

--- src/main/java/edu/onlineFoodApp/dto/User.java
+++ src/main/java/edu/onlineFoodApp/dto/User.java
@@ -15,18 +15,18 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Long id;
 	
 	@NotEmpty(message="User name can't be empty")
 	private String name;	
@@ -48,65 +48,4 @@ public class User {
 	@OneToMany
 	private List<FoodOrders> foodOrders;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public long getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(long phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public List<FoodOrders> getFoodOrders() {
-		return foodOrders;
-	}
-
-	public void setFoodOrders(List<FoodOrders> foodOrders) {
-		this.foodOrders = foodOrders;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phoneNumber=" + phoneNumber + ", password="
-				+ password + ", role=" + role + ", foodOrders=" + foodOrders + "]";
-	}
 }
